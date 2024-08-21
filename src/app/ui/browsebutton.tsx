05301d28e1c3e14e4fe8428a1c2b2b9eb4f5a6d9
Patrick Zhang <patrick8751@gmail.com>
2024-08-21 16:22:27 -0400

    updated browse button
    better visibility for user and style update

--- src/app/ui/browsebutton.tsx
+++ src/app/ui/browsebutton.tsx
@@ -1,4 +1,9 @@
+"use client"
+
 import Link from 'next/link'
+import {pixelify} from '@/app/ui/fonts'
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx'
 
 interface BrowseButtonType {
     pathName: string;
@@ -6,9 +11,11 @@ interface BrowseButtonType {
 }
 
 export default function BrowseButton(props : BrowseButtonType){
+    const pathName = usePathname();
+
     return (
         <Link href={props.pathName}>
-            <div className="w-24 sm:w-28 md:w-32 border-2 border-black py-2 rounded-2xl hover:bg-purple-300 text-center text-md sm:text-xl md:text-2xl">
+            <div className={clsx(`${pixelify.className} w-24 sm:w-28 md:w-32 border-2 border-black py-2 rounded-2xl text-center text-md sm:text-xl md:text-2xl hover:font-bold hover:text-red-500 hover:border-red-500`, {"border-red-500 text-red-500": pathName === props.pathName})}>
                 {props.buttonName}
             </div>
         </Link>
