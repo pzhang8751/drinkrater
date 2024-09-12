c7246d5695bac030a2fa13a00902cdce6eab00fe
Patrick Zhang <patrick8751@gmail.com>
2024-09-12 15:43:53 -0400

    got review base functionality working
    finally got the review button to work as intended

---
+++ src/app/ui/reivewbutton.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import Popup from "reactjs-popup";
+import { RxCross1 } from "react-icons/rx";
+import { useState } from "react";
+import TagForm from "@/app/ui/tagform";
+import { pixelify } from "./fonts";
+
+export default function ReviewButton({params} : {params:{name:string}}) {
+    const [open, setOpen] = useState(false);
+    const closeWindow = () => setOpen(false);
+    const openWindow = () => setOpen(true);
+    
+    async function submitReview() {
+        console.log(document.getElementById("Parties")?.getAttribute("data-value"))
+
+        await fetch('/api/submit-review', {
+            method: 'POST',
+            body: JSON.stringify({
+                "name" : params.name, 
+                "stars" : 5,
+                "tags" : "none"
+            })
+        })
+        
+        closeWindow()
+    }
+
+    function reviewWindow(drinkName: string) {
+        return (
+            <Popup modal open={open}>
+                <div className="h-screen w-screen bg-black bg-opacity-50 grid justify-center">
+                    <div className="h-3/4 w-72 sm:w-128 md:w-160 bg-white self-center rounded-lg flex flex-col overflow-y-auto">
+                        <div className="mt-1 ml-1">
+                            <RxCross1 onClick={closeWindow} className="hover:text-red-500 hover:cursor-pointer" size="20" />
+                        </div>
+                        <p className="mt-2 font-bold self-center text-lg sm:text-xl md:text-2xl">{drinkName}</p>
+                        <hr className="w-5/6 my-1 border-black border-1 self-center" />
+                        <div className="ml-2">
+                            {TagForm()}
+                        </div>
+                        <div className="flex-auto"></div>
+                        <button onClick={submitReview} className={`${pixelify.className} w-32 mr-2 mb-2 place-self-end border-2 text-lg  border-black hover:border-red-500 hover:text-red-500 hover:font-bold`}>Submit</button>
+                    </div>
+                </div>
+            </Popup>
+        );
+    }
+
+    return (
+        <>
+            <button type="button" onClick={openWindow} className={`${pixelify.className} w-32 border-2 border-black hover:border-red-500 hover:text-red-500 hover:font-bold`}>Review</button>
+            {reviewWindow(params.name)}
+        </>
+        
+    );
+}
--- src/app/ui/tagform.tsx
+++ src/app/ui/tagform.tsx
@@ -1,32 +1,8 @@
-'use client'
 
 import drinkData from '@/app/drinkdata.json';
-import { useFormState } from 'react-dom';
 import Tag from './tag';
 
-export async function FormSubmit(prevState: any, formData: any) {
-    const res = await fetch('http://localhost:3000/api', {
-        method: 'POST',
-        body: formData
-    })
-
-    const data = await res.json()
-    // const value = formData.get("test")
-    // console.log(value)
-
-
-    // const value = formData.get("Parties");
-    // console.log(`${value}` + " Hello");
-    // console.log(data);
-    // console.log("hello")
-
-    // const value2 = document.getElementById("HELLO")?.getAttribute("data-value")
-    // console.log(value2)
-    return data
-}
-
 export default function TagForm() {
-    const [state, FormAction] = useFormState(FormSubmit, '')
 
     return (
         <div className="flex flex-col space-y-2">
