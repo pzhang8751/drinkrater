3aee8d126c559acc6c2d01ee52daec89e0fea297
Patrick Zhang <patrick8751@gmail.com>
2025-06-11 13:49:21 -0400

    drinkcarousel component
    playing around with the animation

--- src/app/components/drinkcarousel.tsx
+++ src/app/components/drinkcarousel.tsx
@@ -1,32 +1,59 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
 
 export default function DrinkCarousel() {
+  // need 2x set of cards that appear on screen
+  // need to calculate how many cards can appear on screen
+  // may need to use useeffect and usestate
+  let screenWidth: number = 0;
+  let cardWidth: number = 200;
 
-    // if i want to do animation where the entire thing cycles through i would need 2x images? that fit on screen = 12 for full screen because the slides would need to go through 1 set of images and then another set while displaying one set - both sets have to be the same images
+  if (typeof window !== "undefined") {
+    screenWidth = window.screen.width;
+    document.documentElement.style.setProperty(
+      "--window-size",
+      `${screenWidth}px`
+    );
+  }
+
   return (
-    <div className="pt-6 flex flex-row w-full overflow-x-hidden gap-x-10">
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
+    <div className="relative w-full overflow-x-hidden pt-6 pb-6">
+      <div className="flex flex-row gap-x-10 animate-carousel">
+        {/* hover:[animation-play-state:paused] */}
+        <Card width={cardWidth}></Card>
+        <Card width={cardWidth}></Card>
+        <Card width={cardWidth}></Card>
+        <Card width={cardWidth}></Card>
+        <Card width={cardWidth}></Card>
+
+        {/* <Card></Card> */}
+        {/* 
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card> */}
+      </div>
     </div>
   );
 }
 
-function Card() {
+function Card({ width }: { width: number }) {
   return (
-    <div className="min-h-max min-w-max flex-col px-3 py-3 border-black border transition hover:-translate-y-4 hover:shadow-xl shadow-black ease-in-out">
+    // <Link href="/">
+    <div className="min-h-max min-w-max flex-col px-3 py-3 border-black border transition hover:-translate-y-4 hover:shadow-lg shadow-black">
       <Image
         src="/drinkImages/Coca-Cola.jpg"
         alt="image of drink"
-        width={200}
+        width={width}
         height={400}
         className="border border-black mb-3"
       />
       <p>Brand</p>
       <p className="font-semibold">Drink Name</p>
     </div>
+    // </Link>
   );
 }
