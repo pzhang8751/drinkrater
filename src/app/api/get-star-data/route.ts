d68741f24ec99a384606b343e633a2fe5fcb3a77
Patrick Zhang <patrick8751@gmail.com>
2025-06-15 17:24:57 -0400

    reverted back to api route
    get star data revert to api for client side fetching

---
+++ src/app/api/get-star-data/route.ts
@@ -0,0 +1,37 @@
+import { MongoClient } from "mongodb";
+import { NextResponse, NextRequest } from "next/server";
+import { env } from "process";
+
+const uri = env.MONGODB_URL;
+let client: MongoClient;
+if (uri) {
+  client = new MongoClient(uri);
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    await client.connect();
+
+    const db = client.db("main_data").collection("drink_collection");
+    const drink = request.nextUrl.searchParams.get("drink");
+    const data = await db.findOne({ name: drink });
+
+    let averageStars = 0; 
+
+    if (data !== null) {
+      const stars = data.total_reviews;
+      const reviews = data.total_reviews;
+
+      // calculating average stars roudned to nearest half
+      averageStars = Math.round((stars / reviews) * 2) / 2;
+
+      averageStars = isNaN(averageStars) ? 0 : averageStars;
+    }
+
+    return NextResponse.json(averageStars);
+  } catch {
+    return NextResponse.json({ error: "Internal Server error" });
+  } finally {
+    await client.close();
+  }
+}
--- src/app/components/stardisplay.tsx
+++ src/app/components/stardisplay.tsx
@@ -1,57 +1,20 @@
-"use client"
-
-import { useState, useEffect } from "react";
 import { RiStarFill, RiStarHalfFill, RiStarLine } from "react-icons/ri"
-import { fetchStarData } from "@/lib/data";
 
-interface Params {
-    name: string
-    update: boolean
-}
+export default function StarDisplay({ stars }: { stars: number }) {
+    const size = 40; 
+  let starArray = [];
 
-export default function StarDisplay({ name, update }: Params) {
-    const size = "40";
-
-    const [averageStars, setAverageStars] = useState(0)
-    const [starArray, setStarArray] = useState<JSX.Element[]>([]);
-
-    const fetchData = async () => {
-        const data = await fetchStarData(name)
-        console.log(data + " DATA")
-        if (data===undefined) {
-            setAverageStars(0)
-        } else {
-            setAverageStars(data)
-        }
+  for (let i = 5; i > 0; i--, stars--) {
+    if (stars >= 1) {
+      starArray.push(<RiStarFill key={"stars_" + stars} size={size} />);
+    } else if (stars > 0) {
+      starArray.push(
+        <RiStarHalfFill key={"stars_" + stars} size={size} />
+      );
+    } else {
+      starArray.push(<RiStarLine key={"stars_" + stars} size={size} />);
     }
+  }
 
-    useEffect(() => {
-        fetchData()
-    }, [update])
-
-    useEffect(() => {
-        const newStarArray: JSX.Element[] = [];
-
-        let starData = averageStars
-        /** Using a loop to determine how filled in the next star should be */
-        for (let i = 5; i > 0; i--, starData--) {
-            if (starData >= 1) {
-                newStarArray.push(<RiStarFill key={"stars_" + starData} size={size} />)
-            } else if (starData > 0) {
-                newStarArray.push(<RiStarHalfFill key={"stars_" + starData} size={size} />)
-            } else {
-                newStarArray.push(<RiStarLine key={"stars_" + starData} size={size} />)
-            }
-        }
-
-        setStarArray(newStarArray)
-    }, [averageStars])
-
-    return (
-        <>
-            <div className="mt-2 flex flex-row">
-                {starArray}
-            </div>
-        </>
-    )
+  return <div className="flex flex-row">{starArray}</div>;
 }
