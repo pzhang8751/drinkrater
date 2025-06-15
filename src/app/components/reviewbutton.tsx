4d4ae2e30e3964177a80ce50f9ee99bfc7f6066a
Patrick Zhang <patrick8751@gmail.com>
2025-06-14 20:48:03 -0400

    new review button
    revamping review button and review form from scratch

---
+++ src/app/components/reviewbutton.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { useState } from "react";
+import React from "react";
+import Form from "next/form";
+
+type PopUp = {
+  name: string;
+  isOpen: boolean;
+  closeWindow: () => void;
+};
+
+export default function ReviewButton({ name }: { name: string }) {
+  const [open, setOpen] = useState(false);
+  console.log(open);
+
+  return (
+    <React.Fragment>
+      <button
+        onClick={() => {
+          setOpen(true);
+        }}
+        className="p-2 text-xl hover:cursor-pointer text-white bg-black hover:scale-90 transition ease-in-out hover:bg-pink-400"
+      >
+        Review
+      </button>
+      <ReviewForm
+        name={name}
+        isOpen={open}
+        closeWindow={() => {
+          setOpen(false);
+        }}
+      ></ReviewForm>
+    </React.Fragment>
+  );
+}
+
+function ReviewForm({ name, isOpen, closeWindow }: PopUp) {
+  const [stars, setStars] = useState(0);
+  const [comment, setComment] = useState("");
+
+  function resetWindow() {
+    setStars(0);
+    setComment("");
+    closeWindow(); 
+  }
+
+  function onSubmit() {
+    console.log("hello");
+    // fetch("/api/post-review", {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     name: { name },
+    //     stars: { stars },
+    //     comment: { comment },
+    //   }),
+    //   headers: {
+    //     "Content-type": "application/json; charset=UTF-8",
+    //   },
+    // });
+    resetWindow(); 
+  }
+
+  if (isOpen) {
+    return (
+      <React.Fragment>
+        <div
+          className="fixed top-0 left-0 w-screen h-screen bg-black opacity-25 z-10"
+          onClick={resetWindow}
+        ></div>
+        <div className="fixed top-[20%] left-[20%] bg-white w-[60%] h-[60%] z-20 p-2">
+          <div className="flex flex-row w-full">
+            <p className="font-bold text-xl">{name} Review</p>
+            <button
+              onClick={resetWindow}
+              className="py-1 px-2 bg-black text-white text-xl font-bold hover:scale-90 hover:cursor-pointer hover:bg-pink-400 justify-self-end"
+            >
+              X
+            </button>
+          </div>
+
+          <Form action={onSubmit}>
+            <div className="p-2 border">
+              <textarea
+                maxLength={300}
+                className="outline-none w-full resize-none"
+                placeholder="Leave your thoughts here..."
+                value={comment}
+                onChange={(e) => {
+                  setComment(e.currentTarget.value);
+                }}
+              ></textarea>
+              <p className="text-end italic text-gray-500">
+                {comment.length}/300
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="py-2 px-3 text-white bg-black hover:cursor-pointer"
+            >
+              Submit
+            </button>
+          </Form>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
