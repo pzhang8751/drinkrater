4c78ee20a8e45c4930200cf0fa0261f8a967702e
Patrick Zhang <patrick8751@gmail.com>
2025-06-16 16:42:04 -0400

    formatting

--- src/app/components/stardisplay.tsx
+++ src/app/components/stardisplay.tsx
@@ -1,16 +1,13 @@
-import { RiStarFill, RiStarHalfFill, RiStarLine } from "react-icons/ri"
+import { RiStarFill, RiStarHalfFill, RiStarLine } from "react-icons/ri";
 
-export default function StarDisplay({ stars }: { stars: number }) {
-    const size = 40; 
+export default function StarDisplay({ stars, size }: { stars: number, size:number }) {
   let starArray = [];
 
   for (let i = 5; i > 0; i--, stars--) {
     if (stars >= 1) {
       starArray.push(<RiStarFill key={"stars_" + stars} size={size} />);
     } else if (stars > 0) {
-      starArray.push(
-        <RiStarHalfFill key={"stars_" + stars} size={size} />
-      );
+      starArray.push(<RiStarHalfFill key={"stars_" + stars} size={size} />);
     } else {
       starArray.push(<RiStarLine key={"stars_" + stars} size={size} />);
     }
