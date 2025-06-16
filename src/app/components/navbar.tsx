12d33944ff9d78f9cc6c02c9046acfc3cccbc8da
Patrick Zhang <patrick8751@gmail.com>
2025-06-16 17:55:52 -0400

    commented out
    commented out unused components / features

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import clsx from "clsx";
-import drinkData from "@/app/drinkdata.json";
 
 interface Menu {
   open: () => void;
@@ -57,71 +56,71 @@ export default function NavBar() {
   );
 }
 
-function links({ category, action }: SearchLinks) {
-  let array = drinkData.types;
-  if (category === "brand") array = drinkData.brands;
+// function links({ category, action }: SearchLinks) {
+//   let array = drinkData.types;
+//   if (category === "brand") array = drinkData.brands;
 
-  const linkArrayData = [];
+//   const linkArrayData = [];
 
-  /** This is adding 5 random links to the types / brands section by removing a random type/ brand from the array */
-  //THIS IS THE PROBLEM AREA HAHAHAHHAHAHHAHAHAH random things / editing the array mess with server
-  for (let i = 0; i < 5 && array.length > 0; i++) {
-    // linkArrayData[i] = array.splice(Math.floor(Math.random()*array.length),1)[0];
-    //temp fix
-    linkArrayData[i] = array[i];
-  }
+//   /** This is adding 5 random links to the types / brands section by removing a random type/ brand from the array */
+//   //THIS IS THE PROBLEM AREA HAHAHAHHAHAHHAHAHAH random things / editing the array mess with server
+//   for (let i = 0; i < 5 && array.length > 0; i++) {
+//     // linkArrayData[i] = array.splice(Math.floor(Math.random()*array.length),1)[0];
+//     //temp fix
+//     linkArrayData[i] = array[i];
+//   }
 
-  return (
-    <>
-      {linkArrayData.map((data) => {
-        // console.log(data);
-        return (
-          <Link
-            key={"browse_link_" + data}
-            href={"/browse/" + category + "/" + data}
-            className="hover:underline"
-            onClick={action}
-          >
-            {data[0].toUpperCase() + data.slice(1)}
-          </Link>
-        );
-      })}
-    </>
-  );
-}
+//   return (
+//     <>
+//       {linkArrayData.map((data) => {
+//         // console.log(data);
+//         return (
+//           <Link
+//             key={"browse_link_" + data}
+//             href={"/browse/" + category + "/" + data}
+//             className="hover:underline"
+//             onClick={action}
+//           >
+//             {data[0].toUpperCase() + data.slice(1)}
+//           </Link>
+//         );
+//       })}
+//     </>
+//   );
+// }
 
-function DropDown({ open, close, isOpen }: Menu) {
-  return (
-    <div
-      className={
-        "px-5 bg-blue-100 border-b border-black z-10 fixed flex flex-row space-x-5 transition-all ease-in-out overflow-hidden " +
-        (isOpen ? "translate-y-16" : "-translate-y-44")
-      }
-      onMouseOver={open}
-      onMouseLeave={close}
-    >
-      <section className="flex flex-col gap-y-2">
-        <h2 className="mt-1 font-bold">Types</h2>
-        {links({ category: "type", action: close })}
-        <Link
-          href="/browse/type"
-          className="mb-1 font-semibold hover:underline"
-          onClick={close}
-        >
-          All types
-        </Link>
-      </section>
-      <section className="flex flex-col gap-y-2">
-        <h2 className="mt-1 font-bold">Brands</h2>
-        {links({ category: "brand", action: close })}
-        <Link
-          href="/browse/brand"
-          className="mb-1 font-semibold hover:underline"
-          onClick={close}
-        >
-          All brands
-        </Link>
-      </section>
-    </div>
-  );
-}
+// function DropDown({ open, close, isOpen }: Menu) {
+//   return (
+//     <div
+//       className={
+//         "px-5 bg-blue-100 border-b border-black z-10 fixed flex flex-row space-x-5 transition-all ease-in-out overflow-hidden " +
+//         (isOpen ? "translate-y-16" : "-translate-y-44")
+//       }
+//       onMouseOver={open}
+//       onMouseLeave={close}
+//     >
+//       <section className="flex flex-col gap-y-2">
+//         <h2 className="mt-1 font-bold">Types</h2>
+//         {links({ category: "type", action: close })}
+//         <Link
+//           href="/browse/type"
+//           className="mb-1 font-semibold hover:underline"
+//           onClick={close}
+//         >
+//           All types
+//         </Link>
+//       </section>
+//       <section className="flex flex-col gap-y-2">
+//         <h2 className="mt-1 font-bold">Brands</h2>
+//         {links({ category: "brand", action: close })}
+//         <Link
+//           href="/browse/brand"
+//           className="mb-1 font-semibold hover:underline"
+//           onClick={close}
+//         >
+//           All brands
+//         </Link>
+//       </section>
+//     </div>
+//   );
+// }
