7c95cdc08fe402a840eb2075509fab6baa5b9e4c
Patrick Zhang <patrick8751@gmail.com>
2025-06-15 22:46:15 -0400

    responsive css update

--- src/app/components/searchbarhome.tsx
+++ src/app/components/searchbarhome.tsx
@@ -17,7 +17,7 @@ export default function SearchBarHome() {
   };
 
   return (
-    <div className="mt-8 py-2 px-4 w-[60%] border border-black rounded-4xl flex flex-row gap-x-2">
+    <div className="mt-8 py-2 px-4 w-[80%] md:w-[70%] lg:w-[60%] border border-black rounded-4xl flex flex-row gap-x-2">
       <IoIosSearch className="self-center" size={28} />
       <input
         type="text"
@@ -28,7 +28,7 @@ export default function SearchBarHome() {
         }}
         onKeyDown={search}
         placeholder="Begin your search now"
-        className="w-full text-xl outline-none"
+        className="w-full text-md md:text-xl outline-none"
       ></input>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,27 +9,27 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="*:justify-items-center">
-      <section className="h-screen pt-44">
-        <p className="text-xl sm:text-2xl md:text-3xl">Welcome to</p>
-        <h1 className="text-7xl font-bold sm:text-8xl md:text-9xl">
+    <main className="*:justify-items-center text-center">
+      <section className="h-screen pt-32 md:pt-44">
+        <p className="text-xl md:text-3xl">Welcome to</p>
+        <h1 className="text-6xl font-bold md:text-9xl">
           Drinkrater
         </h1>
-        <p className="text-md sm:text-xl md:text-2xl">
+        <p className="text-md md:text-2xl">
           Find your next favorite drink
         </p>
         <SearchBarHome></SearchBarHome>
       </section>
 
       <section className="mt-10">
-        <h2 className="text-7xl font-bold mb-6">
+        <h2 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-6">
           Review Popular Drinks
         </h2>
         <DrinkCarousel></DrinkCarousel>
       </section>
 
       <section className="mt-10">
-        <h2 className="text-7xl font-bold text-center">
+        <h2 className="text-4xl md:text-6xl lg:text-7xl font-bold">
           Try Something New...<br></br>Every Day!
         </h2>
         <p> Coming soon ... </p>
