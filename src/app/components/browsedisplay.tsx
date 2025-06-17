d9ac2daefb446b83527c919d0f835b0183237a09
Patrick Zhang <patrick8751@gmail.com>
2025-06-17 13:53:45 -0400

    remade fetch browse data

--- src/app/components/browsedisplay.tsx
+++ src/app/components/browsedisplay.tsx
@@ -1,19 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
-import { getData } from "@/lib/db";
+import { getBrowseData } from "@/lib/db";
 
-export default async function BrowseDisplay({ search }: { search: string | undefined}) {
-  const data = await getData(search);
+type Drink = {
+  _id: string,
+  name: string,
+  brand: string
+}
+
+export default async function BrowseDisplay({
+  search,
+}: {
+  search: string | undefined;
+}) {
+  const data = await getBrowseData(search);
 
   function cards() {
     if (data != undefined) {
       if (data.length > 0) {
         return (
-          <div className="grow gap-x-10 grid grid-cols-4 gap-y-8 justify-items-center">
-            {data.map((drink) => {
+          <div className="w-full grow gap-x-10 grid grid-cols-1 md:grid-cols-4 gap-y-4 md:gap-y-8 justify-items-center">
+            {data.map((drink: Drink) => {
               return (
                 <Card
-                  key={`card_${drink.name}`}
+                  key={`card_${drink._id}`}
                   brand={drink.brand}
                   name={drink.name}
                 ></Card>
@@ -22,15 +32,17 @@ export default async function BrowseDisplay({ search }: { search: string | undef
           </div>
         );
       }
-      
     }
 
     return <p className="italic">No drinks match search</p>;
   }
 
   return (
-    <div className="flex flex-row overflow-hidden gap-x-12 *:pt-10">
-      <div className="min-w-48 border-r [&>h2]:font-semibold [&>h2]:text-xl">
+    <div className="flex flex-row *:mt-4 *:md:mt-10 pl-4 md:pl-10">
+      <div className="w-16 md:w-48 border-r [&>h2]:font-semibold [&>h2]:text-md [&>h2]:md:text-xl ">
+        <h1 className="font-bold w-16 md:w-48 text-xl md:text-5xl">
+          Filter
+        </h1>
         <h2>Brand</h2>
         <h2>Type</h2>
       </div>
@@ -42,16 +54,16 @@ export default async function BrowseDisplay({ search }: { search: string | undef
 function Card({ brand, name }: { brand: string; name: string }) {
   return (
     <Link href={`/drink/${name}/${brand}/soda`}>
-      <div className="min-h-max w-[200px] flex-col px-3 py-3 border-black border transition hover:-translate-y-3 hover:shadow-md shadow-black">
+      <div className="min-h-max w-48 flex-col px-3 py-3 border-black border transition hover:-translate-y-3 hover:shadow-md shadow-black">
         <Image
           src="/drinkImages/Coca-Cola.jpg"
           alt="image of drink"
           width={200}
           height={400}
-          className="border border-black mb-3"
+          className="border border-black mb-3 w-full"
         />
-        <p>{brand}</p>
-        <p className="w-full font-semibold text-ellipsis truncate">{name}</p>
+        <p className="text-sm">{brand}</p>
+        <p className="w-full font-semibold text-ellipsis truncate text-sm">{name}</p>
       </div>
     </Link>
   );
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,28 +1,14 @@
-import { MongoClient } from "mongodb";
-import { env } from "process";
 
-const uri = env.MONGODB_URL;
-let client: MongoClient;
-if (uri) {
-  client = new MongoClient(uri);
-}
+export async function getBrowseData(search: string | undefined) {
+  // need to change localhost to the actual webpage when i deploy
+  let apiRoute = "http://localhost:3000/api/get-browse-data?"
 
-export async function getData(search: string | undefined) {
-  try {
-    await client.connect();
-
-    const db = client.db("main_data").collection("drink_collection");
-    let query = {};
-
-    if (search !== undefined) {
-      query = { name: { $regex: `${search}`, $options: "i" } };
-    }
-
-    const array = await db.find(query).limit(20).toArray()
-
-    return array; 
-  } catch {
-  } finally {
-    await client.close();
+  if (search !== undefined) {
+    apiRoute += `search=${search}`
   }
+
+  const res = await fetch(apiRoute, {method:"GET"})
+
+  return res.json()
 }
+
